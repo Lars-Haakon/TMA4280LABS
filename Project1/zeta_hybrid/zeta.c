e53d41caf9e9697a1c44c38766063f0b80973f4c
Lars-Håkon Nohr Nystad <lhnystad@stud.ntnu.no>
2017-03-06 23:49:16 +0100

    added hybrid versions

---
+++ Project1/zeta_hybrid/zeta.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+
+#include <mpi.h>
+
+#include "riemann.h"
+
+int my_rank, comm_sz;
+
+int main(int argc, char **argv) {
+	
+	MPI_Init(&argc, &argv);
+	
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	
+	assert(comm_sz%2==0);
+	
+	// send iteration to all processes
+	int n;
+	if(my_rank == 0) {
+		printf("Enter a value :\n");
+		scanf("%d", &n);
+		for(int i = 1; i < comm_sz; i++)
+			MPI_Send(&n, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+	} else {
+		MPI_Recv(&n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	}
+	
+	double start = MPI_Wtime();
+	
+	double pi_computed = riemann(n);
+
+	if(my_rank == 0) {
+		double finish = MPI_Wtime();
+		double pi_expected = 3.1415926535897932;
+		
+		printf("pi_error: %.20f\nElapsed time: %e seconds\n", fabs(pi_expected-pi_computed), finish-start);
+	}
+	
+	MPI_Finalize();
+}
